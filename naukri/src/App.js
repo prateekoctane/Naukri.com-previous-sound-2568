fbd721000b84329d1c01ec8c2a31c893c21f9fe1
prateekoctane <prateekoctane@gmail.com>
2022-11-09 13:00:47 +0530

    reset

--- naukri/src/App.js
+++ naukri/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Navbar } from "./Components/navbar";
+
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
+     
     </div>
   );
 }
