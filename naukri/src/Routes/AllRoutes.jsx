7f1ff5809d659a61991a33873d49d1a32459990b
prateekoctane <prateekoctane@gmail.com>
2022-11-13 13:42:23 +0530

    added more developments to register page

--- naukri/src/Routes/AllRoutes.jsx
+++ naukri/src/Routes/AllRoutes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { EmployerAdmin } from "./EmployerAdmin";
 import { Home } from "./Home";
 import { Login } from "./Login";
+import { Register } from "./Register";
 export function AllRoutes(){
 
     return <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/employer-admin" element={<EmployerAdmin/>} />
+        <Route path="/register" element={<Register/>} />
     </Routes>
 
 }
