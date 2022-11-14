6f5ef16862723786b875fe242165cdeb3b32c462
prateekoctane <prateekoctane@gmail.com>
2022-11-14 15:53:52 +0530

    added some final changes to navbar

--- naukri/src/Components/1_navbar/Navbar.jsx
+++ naukri/src/Components/1_navbar/Navbar.jsx
@@ -8,17 +8,17 @@ import { ForEmployers } from "./ForEmployers";
 import { NaukriImage } from "./NaukriImage";
 import { Link } from "react-router-dom";
 export function Navbar() {
-    return (<Box display="flex" border="1px" overflow="hidden" w="100%">
+    return (<Box display="flex" overflow="hidden" w="100%" boxShadow="lg" pb="20px" pt="20px">
 
          <Link to="/"><NaukriImage/></Link>
 
-        <Box display="flex" marginLeft="200px" justifyContent="space-between" border="1px" w="250px">
+        <Box display="flex" marginLeft="200px" justifyContent="space-between" w="250px">
             <Jobs />
             <Companies />
             <Services />
         </Box>
 
-        <Box display="flex" marginLeft="500px" border="1px" w="200px" justifyContent="space-around">
+        <Box display="flex" marginLeft="500px" w="200px" justifyContent="space-around">
             <LoginButton /> 
             <Link to="/register"><RegisterButton /></Link>
         </Box>
