0e562f50db2d577aaa7e6fc0347562864eb22200
prateekoctane <prateekoctane@gmail.com>
2022-11-16 08:53:15 +0530

    added some changes to login

--- naukri/src/Contexts/LoginContext.jsx
+++ naukri/src/Contexts/LoginContext.jsx
@@ -4,7 +4,7 @@ export const LoginContext = createContext();
 
 export function LoginContextProvider({children}){
 
-    const [isAuth, setIsAuth ] = useState(false);
+    const [ isAuth, setIsAuth ] = useState(false);
     const [ form, setForm ] = useState({ email:"", password:"" })
 
     return <LoginContext.Provider value={{isAuth,setIsAuth,form,setForm}}>{children}</LoginContext.Provider>
